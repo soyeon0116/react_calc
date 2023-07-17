865fb8bb9eeed5ce8457ba4dd55067183db4fb88
soyeon0116 <ksy9252@naver.com>
2023-07-17 21:24:10 +0900

    design: component 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from "./components/ExpenseList";
+
 class App extends Component {
   render() {
     return (
       <main className="main-container">
         <h1>예산 계산기</h1>
         <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
-
+          <ExpenseForm/>
         </div>
         <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
-          
+        <ExpenseList/>
         </div>
 
         <div style={{display: 'flex',justifyContent:'end',marginTop:'1rem'}}>
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ExpenseItem extends Component {
+  render() {
+    return (
+      <div>ExpenseItem</div>
+    )
+  }
+}
+
+export default ExpenseItem
