cf64dfdbc63d4635b31e1d10cc9b5c015b0ed170
soyeon0116 <ksy9252@naver.com>
2023-07-27 21:40:22 +0900

    feat : 지출목록 추가 기능

--- src/App.js
+++ src/App.js
@@ -5,12 +5,41 @@ import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
   
+  const [charge,setCharge] = useState('');
+
+  const [amount, setamount] = useState(0)
+
   //[변수 이름 getter, state를 정하는 함수 setter] = useState리액트 내장(value, setValue를 리턴하고 초기 state를 정하는 Hook)
   const [expenses, setExpenses] = useState([
     { id: 1, charge:'렌트비',amount : 1600},
     { id: 2, charge:'교통비',amount : 400},
     { id: 3, charge:'식비',amount : 1200}
   ])
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value)
+  }
+
+  const handleAmount = (e) =>{
+    setamount(e.target.valueAsNumber)
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(charge !=="" && amount !== "")
+    {
+      const newExpense = { id: crypto.randomUUID(),charge,amount};
+      //불변성 지켜주기 위해서 새로운 expenses 생성
+      const newExpenses = [...expenses,newExpense];
+      setExpenses(newExpenses);
+      setCharge('');
+      setamount(0);
+    }
+    else
+    {
+      alert('error')
+    }
+  }
 
   const handleDelete = (id) =>{
     //const newExpenses = this.initialExpense.filter(expense => expense.id !== expense)
@@ -24,7 +53,13 @@ const App = () => {
       <main className="main-container">
         <h1>예산 계산기</h1>
         <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
-          <ExpenseForm/>
+          <ExpenseForm
+            handleCharge = {handleCharge}
+            charge={charge}
+            handleAmount = {handleAmount}
+            amount = {amount}
+            handleSubmit ={handleSubmit}
+          />
         </div>
         <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
         <ExpenseList 
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,22 +3,25 @@ import './ExpenseForm.css'
 import { MdSend } from 'react-icons/md';
 
 //함수형 컴포넌트
-const ExpenseForm = () => {
+const ExpenseForm = ({handleCharge,charge,handleAmount,amount,handleSubmit}) => {
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='form-center'>
             <div className='form-group'>
                 <label htmlFor='charge'>지출 항목</label>
-                <input type="text" className='form-control' id='charge' name='charge' placeholder='예)렌트비'/>
+                <input type="text" className='form-control' id='charge' name='charge' placeholder='예)렌트비' value={charge} onChange={handleCharge}/>
             </div>
             <div className='form-group'>
                 <label htmlFor='amount'>지출 항목</label>
-                <input type="number" className='form-control' id='amount' name='amount' placeholder='100'/>
+                <input type="number" className='form-control' id='amount' name='amount' placeholder='100' value={amount} onChange={handleAmount}/>
             </div>
         </div>
         <button type='submit' className='btn'>제출<MdSend className='btn-icon'/></button>
       </form>
     )
 }
-
+//onChange={() =>{
+//  handleCharge()
+//}}
+// = onChange ={handelCharge}
 export default ExpenseForm
