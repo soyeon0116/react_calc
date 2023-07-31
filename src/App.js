b385b5ca13960b7d6a7cf5b8c9b1830b963f260d
soyeon0116 <ksy9252@naver.com>
2023-07-31 21:15:36 +0900

    feat : alert 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from "./components/ExpenseList";
+import { Alert } from "./components/Alert";
 
 const App = () => {
   
   const [charge,setCharge] = useState('');
 
-  const [amount, setamount] = useState(0)
+  const [amount, setamount] = useState(0);
+
+  const [alert, setAlert] = useState({show:false})
 
   //[변수 이름 getter, state를 정하는 함수 setter] = useState리액트 내장(value, setValue를 리턴하고 초기 state를 정하는 Hook)
   const [expenses, setExpenses] = useState([
@@ -34,10 +37,12 @@ const App = () => {
       setExpenses(newExpenses);
       setCharge('');
       setamount(0);
+      handleAlert({type:'success',text:'아이템이 생성되었습니다.'})
     }
     else
     {
       alert('error')
+      handleAlert({type:'danger',text:'charge는 빈 값일 수 없으며 amount는 0보다 커야 합니다.'})
     }
   }
 
@@ -47,10 +52,19 @@ const App = () => {
     //setState() 원래 데이터 : 변경 된 데이터
     console.log(newExpenses)
     setExpenses(newExpenses)
+    handleAlert({type:'danger',text:'아이템이 삭제되었습니다.'})
+  }
+
+  const handleAlert = ({type,text}) =>{
+    setAlert({show:true,type,text});
+    setTimeout(()=>{
+      setAlert({show:false})
+    },7000)
   }
 
     return (
       <main className="main-container">
+        {alert.show ? <Alert type={alert.type} text={alert.text}/> : null}
         <h1>예산 계산기</h1>
         <div style={{width:'100%', backgroundColor:'white',padding:'1rem'}}>
           <ExpenseForm
